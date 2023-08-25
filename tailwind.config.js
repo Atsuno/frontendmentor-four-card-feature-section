4dbc502824e225f31fc9000404d9cd45723e4a96
Atsuno <achiptsuno@gmil.com>
2023-08-25 16:11:24 +0700

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        primaly: 'hsl(234, 12%, 34%)',
+        secondly: 'hsl(229, 6%, 66%)',
+        boxred: 'hsl(0, 78%, 62%)',
+        boxcyan: 'hsl(180, 62%, 55%)',
+        boxorange: 'hsl(34, 97%, 64%)',
+        boxblue: 'hsl(212, 86%, 64%)',
+        rear: 'hsl(0, 0%, 98%)',
+        
+      }
+    },
   },
   plugins: [],
 }
-
